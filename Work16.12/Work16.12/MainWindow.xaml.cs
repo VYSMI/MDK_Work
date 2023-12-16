bd05259f5e570c7ebbf5d79f8452456f1a98504a
VYSMI <151821833+VYSMI@users.noreply.github.com>
2023-12-16 13:53:26 +0900

    Add files via upload

---
+++ Work16.12/Work16.12/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work16._12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int count = 4;
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            lBoxTV.Items.Insert(count,txtBox.Text);
+            count++;
+        }
+
+        private void ButtonAdd1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show($"Результат суммы:{Convert.ToInt32(txtNum1.Text) + Convert.ToInt32(txtNum2.Text)}","Сообщение");
+        }
+    }
+}
